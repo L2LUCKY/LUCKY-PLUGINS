746faefdc692acb3d7023e553af1483e4b0403cd
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-08-06 06:27:43 +0530

    Update history.py

--- history.py
+++ history.py
@@ -32,8 +32,8 @@ async def sg(client: Client, message: Message):
             return await lol.edit("<code>Please specify a valid user!</code>")
     sgbot = ["sangmata_bot", "sangmata_beta_bot"]
     sg = random.choice(sgbot)
-    VIP = random.choice(assistants)
-    ubot = await get_client(VIP)
+    Anony = random.choice(assistants)
+    ubot = await get_client(Anony)
 
     try:
         a = await ubot.send_message(sg, f"{user.id}")
